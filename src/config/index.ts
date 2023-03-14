8f16fc16223493fb86fa1b1e2941c378b03e9882
Hugo Huete <hugo.huete0402@gmail.com>
2023-03-13 18:48:13 -0600

    Changed name of config.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,14 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-const config = {
+type Config ={
+  port:string,
+  database:string | undefined,
+  dbUser:string | undefined,
+  dbPassword:string | undefined
+}
+
+const config:Config = {
   port: process.env.PORT || '3000',
   database: process.env.DATABASE,
   dbUser: process.env.USER,
