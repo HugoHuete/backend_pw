554379bd422112f04e39b4b31713785cfebb3282
Hugo Huete <hugo.huete0402@gmail.com>
2023-03-20 21:54:44 -0600

    Checking if id exists in a model is now implemented in the controllers

--- src/helpers/db-validator.ts
+++ src/helpers/db-validator.ts
@@ -18,40 +18,14 @@ const nameNotExists = async (name: unknown, model: ModelStatic<Model>) => {
   }
 };
 
-const idExists = async (id: unknown, model: ModelStatic<Model>) => {
-  // Check if id is given
-  if (!id) {
-    throw new Error('El id es requerido.');
-  }
-
-  // Check if number
-  const idAsNum = Number(id);
-  if (isNaN(idAsNum)) {
-    throw new Error('El id tiene que ser de tipo numerico.');
-  }
-  // Check if id is in db
-  const entity = await model.findByPk(idAsNum);
-  if (entity) {
-    return;
-  }
-  throw new Error('El id de ese proveedor no existe');
-};
-
-
 // Supplier checks
-const supplierIdExists = async (id: unknown) => {
-  await idExists(id, Supplier);
-};
 const supplierNameNotExists = async (id: unknown) => {
   await nameNotExists(id, Supplier);
 };
 
 // Purchase checks
-const purchaseIdExists = async (id: unknown) => {
-  await idExists(id, Purchase);
-};
 const purchaseNameNotExists = async (id: unknown) => {
   await nameNotExists(id, Purchase);
 };
 
-export { supplierIdExists, supplierNameNotExists, purchaseIdExists, purchaseNameNotExists };
+export { supplierNameNotExists, purchaseNameNotExists };
