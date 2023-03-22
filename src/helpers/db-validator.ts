dc6e3ad82dd7b7d136b5f269cf096973334f09a9
Hugo Huete <hugo.huete0402@gmail.com>
2023-03-22 00:41:44 -0600

    Created a custom validator for dates using luxon

--- src/helpers/db-validator.ts
+++ src/helpers/db-validator.ts
@@ -1,5 +1,17 @@
 import { Purchase, Supplier } from '../models';
 import { Model, ModelStatic } from 'sequelize';
+import { DateTime } from 'luxon';
+
+
+const checkDateFormat = (date:string) => {
+  // yyyy-mm-dd
+  const dateFormatted = DateTime.fromISO(date);
+
+  if (!dateFormatted.isValid ) {
+    throw new Error('Invalid date.');
+  }
+  return true;
+};
 
 const nameNotExists = async (name: unknown, model: ModelStatic<Model>) => {
   // If not name or name is not a string, dont check anything more
@@ -28,4 +40,4 @@ const purchaseNameNotExists = async (id: unknown) => {
   await nameNotExists(id, Purchase);
 };
 
-export { supplierNameNotExists, purchaseNameNotExists };
+export { supplierNameNotExists, purchaseNameNotExists, checkDateFormat };
