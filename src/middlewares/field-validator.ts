6ca2895a5c477a8d4c1e2f59e8c79303e5bb5a9a
Hugo Huete <hugo.huete0402@gmail.com>
2023-03-22 00:42:15 -0600

    Formatted error display to be {status, errors[], error}

--- src/middlewares/field-validator.ts
+++ src/middlewares/field-validator.ts
@@ -5,7 +5,9 @@ export const fieldValidator = (req:Request, res:Response, next:NextFunction) => 
   // Check the errors returned by 'check' of express-validator
   const errors = validationResult(req);
   if (!errors.isEmpty()){
-    return res.status(400).json(errors);
+    const errorsArray = errors.array();
+    const data = errorsArray.map(error => error.msg);
+    return res.status(400).json({status:400, data, error:true});
   }
   return next();
 };
